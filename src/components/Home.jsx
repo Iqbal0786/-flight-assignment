345df6826ecdad259b3fc384b0e30d1fb370389c
Mohammad Iqbal <iqbal240456@gmail.com>
2022-04-28 18:06:50 +0530

    initial setup is done

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import { AppBar, Toolbar, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+
+export default function Home() {
+  return (
+<>
+<AppBar>
+    <Toolbar>
+        <Typography>Welcome to My  Flight </Typography>
+    </Toolbar>
+</AppBar>
+<Box>
+    
+</Box>
+</>
+  )
+}
